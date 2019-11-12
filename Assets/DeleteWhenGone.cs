b502c876e27c1fc6d20a0c34a87e654d923b6d6c
Danilo Gasques <danilod100@gmail.com>
2019-11-12 09:36:40 -0800

    First commit

---
+++ Assets/DeleteWhenGone.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteWhenGone : MonoBehaviour
+{
+    [Tooltip("How far down can it go before getting destroyed")]
+    public float maxDistance = 100f;
+
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y <= -maxDistance)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/WhiteSphereShooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+
+public class WhiteSphereShooter : MonoBehaviour
+{
+
+    public Transform WhiteSpherePrefab;
+
+    public void OnAirTap()
+    {
+        Transform newSphereTransform = Object.Instantiate(WhiteSpherePrefab);
+        newSphereTransform.position = Camera.main.transform.position;
+        Rigidbody r = newSphereTransform.GetComponent<Rigidbody>();
+
+        if (r != null)
+        {
+            r.velocity = CoreServices.InputSystem.GazeProvider.GazeDirection.normalized * 10.0f;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnAirTap();
+        }
+    }
+
+    
+}
